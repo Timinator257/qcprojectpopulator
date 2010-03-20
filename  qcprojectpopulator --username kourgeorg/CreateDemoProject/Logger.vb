939df4f600e1f25197f34a58ce55b5f2b3ce3caa
kourgeorge <kourgeorge@7c4035d4-1679-11df-96eb-2d7817107f39>
2010-03-20 12:44:49 +0000

    split to modules

---
+++  qcprojectpopulator --username kourgeorg/CreateDemoProject/Logger.vb
@@ -0,0 +1,56 @@
+﻿Module Logger
+    Public LogFileName As String
+    Public LogFile As System.IO.StreamWriter
+    Public log As String
+    Public LogState As Boolean 'Determines wheher to redirect Result.Text to log file
+
+    Public Sub InitializeLogFile()
+        'Create the log file in the filesystem
+        Dim ActualLogFileName As String
+        LogFileName = Form.LogTextBox.Text
+        ActualLogFileName = LogFileName
+        Dim k = 1
+        While System.IO.File.Exists(ActualLogFileName)
+            ActualLogFileName = LogFileName.Insert(LogFileName.IndexOf("."), "(" & k.ToString & ")")
+            k += 1
+        End While
+
+        LogFile = New System.IO.StreamWriter(ActualLogFileName, True)
+        LogFile.WriteLine("<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.01 Transitional//EN"" ""http://www.w3.org/TR/html4/loose.dtd"">")
+        LogFile.WriteLine("<html>")
+        LogFile.WriteLine("<head>")
+        LogFile.WriteLine("<title>QC Project Data Populator Logger</title>")
+        LogFile.WriteLine("<p style=""color: #3A3A3A; font-size: xx-large; font-weight: bold; font-style: italic"">QC Project Data Populator</p>")
+        LogFile.WriteLine("<p style=""color: #224466; font-size: medium; font-weight: bold"">HP Software R&D Internal</p>")
+        LogFile.WriteLine("<style type=""text/css"">")
+        LogFile.WriteLine("</style>")
+        LogFile.WriteLine("</head>")
+        LogFile.WriteLine("<body bgcolor=""#FFFFFF"" topmargin=""6"" leftmargin=""6"">")
+        LogFile.WriteLine("<hr size=""1"" noshade>")
+        LogFile.WriteLine("Log session start time: " & LoginTime & "<br><br>")
+        LogFile.WriteLine("<table cellspacing=""0"" cellpadding=""4"" border=""1"" bordercolor=""#224466"" width=""100%"">")
+        LogFile.WriteLine("<tr> <th>Time</th><th>Category</th><th>Message</th></tr>")
+
+    End Sub
+
+
+    Public Sub AddEventToLog(ByVal Category As Integer, ByVal Message As String)
+        If Form.LoggerCheckBox.Checked Then
+            LogFile.WriteLine("<tr>")
+            Dim NowTime = Now.TimeOfDay.ToString()
+            If NowTime.Length >= 12 Then
+                NowTime = NowTime.Substring(0, 12)
+            End If
+            Dim NowDay = Now.Date.ToString.Substring(0, 10)
+            LogFile.WriteLine("<td>" & NowDay & "  " & NowTime & "</td>")
+            If Category = 1 Then
+                LogFile.WriteLine("<td title=""Category"">" & "Info" & "</td>")
+            Else
+                LogFile.WriteLine("<td title=""Category"" style=""color: #0F73A1"">" & "Performance" & "</td>")
+            End If
+            LogFile.WriteLine("<td title=""Message"">" & Message & "</td>")
+            LogFile.WriteLine("</tr>")
+        End If
+    End Sub
+
+End Module
---
+++  qcprojectpopulator --username kourgeorg/CreateDemoProject/Test.vb
@@ -0,0 +1,116 @@
+﻿Module Test
+    Public TestsList As TDAPIOLELib.List
+
+    Public Sub Handle_Tests()
+
+        Dim test1 As TDAPIOLELib.Test
+        Dim testF As TDAPIOLELib.TestFactory
+        Dim TestCounter = 0
+
+        Dim treeMng As TDAPIOLELib.TreeManager
+        Dim oRoot, folder As TDAPIOLELib.SubjectNode
+        treeMng = tdc.TreeManager
+        oRoot = treeMng.TreeRoot("Subject")
+
+        Dim list1 As Queue(Of TDAPIOLELib.SubjectNode) = New Queue(Of TDAPIOLELib.SubjectNode)
+        list1.Clear()
+        list1.Enqueue(oRoot)
+        For j = 1 To Val(Form.TestDirLevels.Text)  'for each level do
+            Dim list2 As Queue(Of TDAPIOLELib.SubjectNode) = New Queue(Of TDAPIOLELib.SubjectNode)
+            list2.Clear()
+            For d = 1 To list1.Count          ' for each folder in list1
+
+                Dim currfold As TDAPIOLELib.SubjectNode
+                currfold = list1.Dequeue()    ' remove the folder from list1
+
+                For i = 1 To Val(Form.TestDirsInLevel.Text)
+                    If currfold.NodeID = oRoot.NodeID Then
+                        folder = currfold.AddNode("Subfolder " & i.ToString & " of Folder " & currfold.NodeID.ToString & " at " & LoginTime)
+                    Else
+                        folder = currfold.AddNode("Subfolder " & i.ToString & " of Folder " & currfold.NodeID.ToString)
+                    End If
+                    folder.Post()
+                    AddEventToLog(1, "Creating Test Folder " & folder.Name)
+                    list2.Enqueue(folder)
+                    For k = 1 To Val(Form.TestsInLevel.Text)
+                        testF = folder.TestFactory
+                        test1 = testF.AddItem(System.DBNull.Value)
+                        test1.Name = "Test " & k.ToString
+                        test1.Post()
+                        TestsList.Add(test1)
+                        AddEventToLog(1, "Creating Test " & test1.ID)
+                        'Handle Steps
+                        For s = 1 To Val(Form.StepsTextBox.Text)
+                            Dim Designstep As TDAPIOLELib.DesignStep
+                            Dim DesignStepF As TDAPIOLELib.DesignStepFactory
+                            DesignStepF = test1.DesignStepFactory
+                            Designstep = DesignStepF.AddItem(DBNull.Value)
+                            Designstep.StepName = "Step " & s
+                            Designstep.StepDescription = "Step " & s & "of Test " & test1.ID.ToString
+                            Designstep.StepExpectedResult = "Expected Result of step " & s
+                            Designstep.Post()
+                        Next
+                        'Handle Attachments
+                        TestCounter += 1
+                        If Form.AttachmentCheckBox.Checked Then
+                            If TestCounter <= Val(Form.TestPlanAttachement.Text) Then
+                                Dim attachF As TDAPIOLELib.AttachmentFactory = test1.Attachments
+                                Dim attachment As TDAPIOLELib.Attachment = attachF.AddItem(DBNull.Value)
+                                attachment.FileName = Form.AttachementTextBox.Text
+                                attachment.Type = TDAPIOLELib.TDAPI_ATTACH_TYPE.TDATT_FILE
+                                attachment.Post()
+                                AddEventToLog(1, "Adding Attachment for Test " & test1.ID)
+                            End If
+                        End If
+                    Next
+                Next
+            Next
+            list1 = list2
+        Next
+
+    End Sub
+
+    Public Sub Handle_Tests_defects_linkage()
+        Dim defectsNum As Long
+        Dim Rand As Long
+        Dim defect As TDAPIOLELib.Bug
+        Dim test As TDAPIOLELib.Test
+        Dim testNum As Long
+
+        defectsNum = DefectsList.Count
+        testNum = TestsList.Count
+
+        Dim ilink As TDAPIOLELib.ILinkable
+        Dim linkF As TDAPIOLELib.LinkFactory
+        Dim link As TDAPIOLELib.Link
+
+        Dim NumOfMaxLinks = Int(((defectsNum) * testNum))
+        If (Val(Form.TestDefectLNK.Text)) > NumOfMaxLinks Then
+            Throw New Exception("Given " & defectsNum.ToString & " Defects and " & testNum.ToString & " Test, Number of max Linkage is " & NumOfMaxLinks.ToString & vbCrLf & "Refresh your combinatorics")
+        End If
+
+        For i = 1 To Val(Form.TestDefectLNK.Text)
+            Randomize()
+            Rand = Int((defectsNum - 1 + 1) * Rnd()) + 1
+            defect = DefectsList.Item(Rand)
+
+            Randomize()
+            Rand = Int((testNum - 1 + 1) * Rnd()) + 1
+            test = TestsList.Item(Rand)
+
+            ilink = test
+            linkF = ilink.BugLinkFactory
+            link = linkF.AddItem(defect)
+
+            Try
+                link.Post()
+                AddEventToLog(1, "Linking Test " & test.ID & " and Defect " & defect.ID)
+            Catch exp As Exception
+                i = i - 1
+                AddEventToLog(1, "Duplication Found: Randomly chosen entities are already linked , trying again...")
+                Continue For
+            End Try
+
+        Next
+    End Sub
+End Module
